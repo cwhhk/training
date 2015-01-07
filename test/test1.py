c6066bf51c1f8c13128b556478d2f8800baeacfd
cwhhk <cwhhk@sina.com>
2015-01-07 20:36:37 +0800

    练习

--- test/test1.py
+++ test/test1.py
@@ -1,4 +1,8 @@
 __author__ = 'chenwei'
-print ord('A')
-print chr(65)
-print '中文'.encode('utf-8')
+# -*- coding: utf-8 -*-
+print len("ABC")
+print len(u'ABC')
+print len(u'中文')
+print len('\xe4\xb8\xad\xe6\x96\x87')
+
+print '%s',('\xe4\xb8\xad\xe6\x96\x87')
