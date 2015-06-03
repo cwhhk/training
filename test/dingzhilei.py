a9d582616533c884a49759a38673f7cf35b6adbe
cwhhk <cwhhk@sina.com>
2015-06-03 17:53:07 +0800

    练习

--- test/dingzhilei.py
+++ test/dingzhilei.py
@@ -58,11 +58,70 @@ __author__ = 'chenwei'
 # print s.age()
 # print s.abc
 
+# class Chain(object):
+#     def __init__(self,path=''):
+#         self._path = path
+#     def __getattr__(self, path):
+#         return Chain('%s/%s' % (self._path,path) )
+#     def __str__(self):
+#         return self._path
+# print Chain().status.user.timeline.list
+
+# class Student(object):
+#     def __init__(self,name):
+#         self.name = name
+#     def __call__(self):
+#         print("My name is %s." % self.name)
+#
+# callable(Student(11))
+# callable(max)
+# callable([1,2,3])
+# callable(None)
+# callable("string")
+
+
+# class Chain(object):
+#     def __init__(self,path=''):
+#         self._path = path
+#     def __call__(self,name):
+#         self._call_name = self
+#         self._name = name
+#         return Chain('Get %s/%s' % (self._call_name,self._name) )
+#     def __getattr__(self, path):
+#         return Chain('%s/%s' % (self._path,path) )
+#     def __str__(self):
+#         return self._path
+# print Chain().users('michael').repos
+
 class Chain(object):
-    def __init__(self,path=''):
+
+    def __init__(self, path=''):
         self._path = path
+
+    def getpath(self):
+        return self._path
+
+    def _setpath(self,path,f=0):
+        k=self._path.find('GET');
+        if(not f):
+            if(k==0):
+                self._path=('%s/%s' % (self._path, path))
+            else:
+                self._path=('GET %s/%s' % (self._path, path))
+        else:
+            self._path=('%s/:%s' % (self._path, path))
+
     def __getattr__(self, path):
-        return Chain('%s/%s' % (self._path,path) )
+        self._setpath(path)
+        return self
+
     def __str__(self):
         return self._path
-print Chain().status.user.timeline.list
+
+    def __call__(self,path=''):
+        self._setpath(path,1)
+        return self
+
+    __repr__=__str__
+
+print Chain().users("Macheal").repo
